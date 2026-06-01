8fb46fe0532b9d3dc20d77cd961066513a732546
Parwin Poonsuraphun <parwin@ParwinDeMac-2.local>
2026-06-01 15:17:20 +0800

    Deploy router fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/THE-SILENT-SURFACE">
     <App />
   </BrowserRouter>
 )
